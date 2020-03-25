04e62ae6a27cc01b6790886f1225b2a521ca2a77
BielMaxBR <gbielmaximo@gmail.com>
2020-03-25 18:30:17 -0300

    vo deixar pra amanha flw

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+
+
+import './styles.css'
+
+export default function NewIncident() {
+    return <h1>new incident</h1>
+}
